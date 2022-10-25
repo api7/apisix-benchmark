d5328440202f72f44856f3ae6dbe87d9925c0d35
tzssangglass <tzssangglass@gmail.com>
2022-10-25 04:28:46 +0000

    revert

--- test/case2/apisix/test.sh
+++ test/case2/apisix/test.sh
@@ -28,9 +28,10 @@ trap clean_up EXIT
 sudo docker run --name apisix-standalone \
     -v $PWD/test/case2/apisix/config.yaml:/usr/local/apisix/conf/config.yaml \
     -v $PWD/test/case2/apisix/apisix.yaml:/usr/local/apisix/conf/apisix.yaml \
+    -v /home/tzs/apisix/apisix:/usr/local/apisix/apisix \
     -p 9080:9080 \
     --network=host \
-    -d apache/apisix:2.99.0-debian
+    -d apache/apisix:dev
 
 sleep 3
 
