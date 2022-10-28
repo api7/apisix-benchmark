81007040b0b60064edaf5bd9dc6a6ce02659308a
tzssangglass <tzssangglass@gmail.com>
2022-10-28 16:58:11 +0800

    feat: add scripts and docs (#1)

---
+++ test/case1/kong/test.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ex
+
+clean_up() {
+    sudo docker stop kong-dbless || true
+    sudo docker rm kong-dbless || true
+}
+
+trap clean_up EXIT
+
+sudo docker run -d --name kong-dbless \
+    --network=host \
+    -v $PWD/test/case1/kong/declarative/kong.yaml:/kong/declarative/kong.yaml \
+    -e "KONG_DATABASE=off" \
+    -e "KONG_DECLARATIVE_CONFIG=/kong/declarative/kong.yaml" \
+    -e "KONG_PROXY_ACCESS_LOG=off" \
+    -e "KONG_NGINX_WORKER_PROCESSES=1" \
+    -e "KONG_LOG_LEVEL=warn" \
+    -p 8000:8000 \
+    kong:3.0.0-ubuntu
+
+sleep 3
+
+rm -rf $PWD/test/case1/kong/result || true
+mkdir $PWD/test/case1/kong/result
+
+for i in {1..10}; do \
+    wrk -c100 -t4 -d10 -R99999 -U http://127.0.0.1:8000/hello > $PWD/test/case1/kong/result/$i.log 2>&1
+done
---
+++ test/case3/apisix/test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ex
+
+clean_up() {
+    sudo docker stop apisix-standalone || true
+    sudo docker rm apisix-standalone || true
+}
+
+trap clean_up EXIT
+
+sudo docker run --name apisix-standalone \
+    -v $PWD/test/case3/apisix/config.yaml:/usr/local/apisix/conf/config.yaml \
+    -v $PWD/test/case3/apisix/apisix.yaml:/usr/local/apisix/conf/apisix.yaml \
+    -p 9080:9080 \
+    --network=host \
+    -d apache/apisix:dev
+
+sleep 3
+
+rm -rf $PWD/test/case3/apisix/result || true
+mkdir $PWD/test/case3/apisix/result
+
+for i in {1..10}; do \
+    wrk -c100 -t4 -d10 -R99999 -U http://127.0.0.1:9080/hello  -H "apikey: user-key"> $PWD/test/case3/apisix/result/$i.log 2>&1
+done
